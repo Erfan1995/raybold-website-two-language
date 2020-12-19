9f7ba2b796dc4adcfed9bb9e7d0186e951098248
unknown <erfan1995>
2020-12-19 11:09:30 +0430

    Sdf

--- projects/website/src/app/about-us/about-us.service.ts
+++ projects/website/src/app/about-us/about-us.service.ts
@@ -3,6 +3,7 @@ import {environment as localEnvironment} from '../../environments/environment';
 import {environment as productionEnvironment} from '../../environments/environment.prod';
 import {HttpService} from '../services/http.service';
 import {Observable} from 'rxjs';
+import {GetLangService} from '../services/get-lang.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,13 @@ import {Observable} from 'rxjs';
 
 export class AboutUsService {
   baseUrl = 'api/about-us/';
+  lang;
   private env = localEnvironment || productionEnvironment;
 
-  constructor(private httpServices: HttpService) {
+  constructor(private httpServices: HttpService, private langService: GetLangService) {
+    this.baseUrl += this.langService.getLang() + '/';
+    this.lang = this.langService.getLang() + '/';
+
   }
 
   getAboutUsInfo(): Observable<any> {
@@ -22,8 +27,9 @@ export class AboutUsService {
   FilePath(fileName: string) {
     return this.env.baseUrl.backend.main + 'api/aboutus/' + 'aboutUsFiles/' + fileName;
   }
+
   listCustomersReview(): Observable<any> {
-    return this.httpServices.get('api/home/' + 'list-customers-review');
+    return this.httpServices.get('api/home/' + this.lang + '/list-customers-review');
   }
 
   customerFilePath(fileName: string) {
--- projects/website/src/app/layout/header/header.component.ts
+++ projects/website/src/app/layout/header/header.component.ts
@@ -17,7 +17,6 @@ export class HeaderComponent implements OnInit {
   serviceSubCategory = [];
   index = 0;
   isEqual = true;
-
   constructor(private headerService: HeaderService,
               private router: Router,
               private dynamicScriptLoader: DynamicScriptLoaderService,
